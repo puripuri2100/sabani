a128018892b3de6f545a775c9a00f13b72a49b20
puripuri2100 <puripuri2100@gmail.com>
2025-06-20 22:53:47 +0900

    DOMを表示させるように変更してみた

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use sabani_core::net::HttpClient;
+use sabani_core::renderer::html::{parser::HtmlParser, token::HtmlTokenizer};
+use sabani_core::utils::convert_dom_to_string;
 
 fn main() {
   let client = HttpClient::new();
   let response = client
     .get("localhost".to_string(), 8000, "/test.html".to_string())
     .unwrap();
-  println!("{response:?}");
+  let html = response.body();
+  let t = HtmlTokenizer::new(html);
+  let window = HtmlParser::new(t).construct_tree();
+  let document = window.borrow().document();
+  let document_html_string = convert_dom_to_string(&Some(document));
+  println!("{document_html_string}");
 }
